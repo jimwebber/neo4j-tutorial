ffb6be470919e718e118b90d0486d81637021647
Jim Webber <jim@jimwebber.org>
2011-03-08 18:25:52 +0000

    Adding a list of doctors as the first bit of data; adding the requisite jars.

---
+++ src/test/java/org/neo4j/tutorial/DoctorWhoUniverseTest.java
@@ -0,0 +1,11 @@
+package org.neo4j.tutorial;
+
+import org.junit.Test;
+
+
+public class DoctorWhoUniverseTest {
+    @Test
+    public void seeIfTheJsonLoads() {
+        new DoctorWhoUniverse();
+    }
+}
