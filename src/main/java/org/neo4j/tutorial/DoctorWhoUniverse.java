239a008aed0f956dd416328e97568838fd8b2649
Jim Webber <jim@jimwebber.org>
2011-03-08 18:25:52 +0000

    Adding a list of doctors as the first bit of data; adding the requisite jars.

---
+++ src/main/java/org/neo4j/tutorial/DoctorWhoUniverse.java
@@ -0,0 +1,51 @@
+package org.neo4j.tutorial;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+
+public class DoctorWhoUniverse {
+    private GraphDatabaseService db = new EmbeddedGraphDatabase("DoctorWhoDB");
+
+    public DoctorWhoUniverse() throws RuntimeException {
+        try {
+            loadDoctors(new File("src/main/resources/doctors.json"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loadDoctors(File doctorsData) throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper m = new ObjectMapper();
+        List<ArrayList> doctorsList = m.readValue(doctorsData, List.class);
+        
+        Transaction tx = db.beginTx();
+        try {
+            for(int i = 0; i < doctorsList.size(); i++) {
+                createADoctor(((ArrayList<String>)doctorsList.get(i)).get(0), ((ArrayList<String>)doctorsList.get(i)).get(1), i);
+            }
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+    }
+
+    private void createADoctor(String firstname, String lastname, int incarnation) {
+        Node doctor = db.createNode();
+        doctor.setProperty("firstname", firstname);
+        doctor.setProperty("lastname", lastname);
+        doctor.setProperty("incarnation", incarnation);
+    }
+
+}
