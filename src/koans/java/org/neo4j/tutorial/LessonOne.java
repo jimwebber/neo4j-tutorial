06d36c7a59b962701e1d81620267c96a370975d8
Jim Webber <jim@jimwebber.org>
2011-03-09 15:34:23 +0000

    removed the Doctor Who db files, they should be built by the koans in a temp dir

--- src/koans/java/org/neo4j/tutorial/LessonOne.java
+++ src/koans/java/org/neo4j/tutorial/LessonOne.java
@@ -3,17 +3,19 @@ package org.neo4j.tutorial;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.neo4j.graphdb.DynamicRelationshipType;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
 
 public class LessonOne extends Koan {
-    
+
     @Test
-    public void shouldCreateANodeInTheDatabase() {        
+    public void shouldCreateANodeInTheDatabase() {
         Node node = null;
 
-        //SNIPPET_START
-        
+        // SNIPPET_START
+
         Transaction tx = db.beginTx();
         try {
             node = db.createNode();
@@ -21,34 +23,68 @@ public class LessonOne extends Koan {
         } finally {
             tx.finish();
         }
-        
-        //SNIPPET_END
-        
+
+        // SNIPPET_END
+
         assertTrue(DatabaseHelper.nodeExistsInDatabase(db, node));
     }
-    
+
     @Test
     public void shouldCreateSomePropertiesOnANode() {
-        Node node = null;
+        Node theDoctor = null;
 
-        //SNIPPET_START
-        
+        // SNIPPET_START
+
         Transaction tx = db.beginTx();
         try {
-            node = db.createNode();
-            node.setProperty("firstname", "William");
-            node.setProperty("lastname", "Hartnell");
+            theDoctor = db.createNode();
+            theDoctor.setProperty("firstname", "William");
+            theDoctor.setProperty("lastname", "Hartnell");
             tx.success();
         } finally {
             tx.finish();
         }
-        
-        //SNIPPET_END
-        
-        assertTrue(DatabaseHelper.nodeExistsInDatabase(db, node));
-        
-        Node storedNode = db.getNodeById(node.getId());
+
+        // SNIPPET_END
+
+        assertTrue(DatabaseHelper.nodeExistsInDatabase(db, theDoctor));
+
+        Node storedNode = db.getNodeById(theDoctor.getId());
         assertEquals("William", storedNode.getProperty("firstname"));
         assertEquals("Hartnell", storedNode.getProperty("lastname"));
+    }
+
+    @Test
+    public void shouldRelateTwoNodes() {
+        Node theDoctor = null;
+        Node susan = null;
+        Relationship companionRelationship = null;
+
+        // SNIPPET_START
+
+        Transaction tx = db.beginTx();
+        try {
+            theDoctor = db.createNode();
+            theDoctor.setProperty("firstname", "William");
+            theDoctor.setProperty("lastname", "Hartnell");
+
+            susan = db.createNode();
+            theDoctor.setProperty("firstname", "Susan");
+            theDoctor.setProperty("lastname", "Campbell");
+
+            companionRelationship = susan.createRelationshipTo(theDoctor, DynamicRelationshipType.withName("COMPANION_OF"));
+
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+
+        // SNIPPET_END
+        
+        Relationship storedCompanionRelationship = db.getRelationshipById(companionRelationship.getId());
+        assertNotNull(storedCompanionRelationship);
+        assertEquals(susan, storedCompanionRelationship.getStartNode());
+        assertEquals(theDoctor, storedCompanionRelationship.getEndNode());
+
     }
 }
