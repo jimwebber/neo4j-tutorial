30ee6e18b38548a631f22451d5c1ecd2ec342fb8
Jim Webber <jim@jimwebber.org>
2011-03-09 15:11:26 +0000

    Added the full shell script for removing the answers from the koan

--- src/main/scripts/remove_snippets.sh
+++ src/main/scripts/remove_snippets.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 # Script to remove any code between //SNIPPET_START and //SNIPPET_END in Java unit test files
+
+find src/koans/java -name *.java -print | xargs sed -f src/main/scripts/remove_snippets.sed -i ""
