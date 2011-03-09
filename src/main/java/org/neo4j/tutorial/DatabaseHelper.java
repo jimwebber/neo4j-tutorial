51ab72caf23c100ed346c04256bbbef11e344f1b
Jim Webber <jim@jimwebber.org>
2011-03-09 14:00:08 +0000

    Updated the roadmap of the tutorial to include indexing

---
+++ src/main/java/org/neo4j/tutorial/DatabaseHelper.java
@@ -0,0 +1,56 @@
+package org.neo4j.tutorial;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+
+public class DatabaseHelper {
+
+    public static GraphDatabaseService createDatabase() {
+
+        return new EmbeddedGraphDatabase(createTempDatabaseDir().getAbsolutePath());
+    }
+
+    private static File createTempDatabaseDir() {
+
+        File d;
+        try {
+            d = File.createTempFile("neo4j-test", "dir");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (!d.delete()) {
+            throw new RuntimeException("temp config directory pre-delete failed");
+        }
+        if (!d.mkdirs()) {
+            throw new RuntimeException("temp config directory not created");
+        }
+        d.deleteOnExit();
+        return d;
+    }
+    
+    
+    public static void dumpGraphToConsole(GraphDatabaseService db) {
+        for(Node n : db.getAllNodes()) {
+            Iterable<String> propertyKeys = n.getPropertyKeys();
+            for(String key : propertyKeys) {
+                System.out.print(key + " : " );
+                System.out.println(n.getProperty(key));
+            }
+        }
+    }
+
+    public static int countNodes(Iterable<Node> allNodes) {
+        Iterator<Node> iterator = allNodes.iterator();
+        int count = 0;
+        while(iterator.hasNext()) {
+            count ++;
+            iterator.next();
+        }
+        return count;
+    }
+}
