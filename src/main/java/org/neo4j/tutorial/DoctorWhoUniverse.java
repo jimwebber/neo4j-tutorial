b5cef55fef5cd337a002ddc7017402a129fbd83e
Jim Webber <jim@jimwebber.org>
2011-03-16 23:56:12 +0100

    Added the first java examples to the presentation and sorted out the build so that it brings down the neo4j-index

--- src/main/java/org/neo4j/tutorial/DoctorWhoUniverse.java
+++ src/main/java/org/neo4j/tutorial/DoctorWhoUniverse.java
@@ -8,32 +8,49 @@ import java.util.List;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.neo4j.graphdb.DynamicRelationshipType;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.index.IndexService;
+import org.neo4j.index.lucene.LuceneIndexService;
 
 public class DoctorWhoUniverse {
     private GraphDatabaseService db = DatabaseHelper.createDatabase();
+    private IndexService index = new LuceneIndexService(db);
 
     public DoctorWhoUniverse() throws RuntimeException {
         try {
-            loadDoctors(new File("src/main/resources/doctors.json"));
+            loadAllDoctors(new File("src/main/resources/doctors.json"));
+            indexDoctors();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
 
+    private void indexDoctors() {
+        
+    }
+
+
     @SuppressWarnings({ "unchecked", "rawtypes" })
-    private void loadDoctors(File doctorsData) throws JsonParseException, JsonMappingException, IOException {
+    private void loadAllDoctors(File doctorsData) throws JsonParseException, JsonMappingException, IOException {
 
         ObjectMapper m = new ObjectMapper();
         List<ArrayList> doctorsList = m.readValue(doctorsData, List.class);
         
         Transaction tx = db.beginTx();
         try {
+            Node previousDoctor = null;
             for(int i = 0; i < doctorsList.size(); i++) {
-                createADoctor(((ArrayList<String>)doctorsList.get(i)).get(0), ((ArrayList<String>)doctorsList.get(i)).get(1), i);
+                Node currentDoctor = createADoctor(((ArrayList<String>)doctorsList.get(i)).get(0), ((ArrayList<String>)doctorsList.get(i)).get(1), i);
+                
+                if(previousDoctor != null) {
+                    previousDoctor.createRelationshipTo(currentDoctor, DynamicRelationshipType.withName("REGENERATED_TO"));
+                }
+                
+                previousDoctor = currentDoctor;
             }
             tx.success();
         } finally {
@@ -41,11 +58,17 @@ public class DoctorWhoUniverse {
         }
     }
 
-    private void createADoctor(String firstname, String lastname, int incarnation) {
+    private Node createADoctor(String firstname, String lastname, int incarnation) {
         Node doctor = db.createNode();
         doctor.setProperty("firstname", firstname);
         doctor.setProperty("lastname", lastname);
         doctor.setProperty("incarnation", incarnation);
+        
+        return doctor;
+    }
+
+    public GraphDatabaseService getDoctorWhoDatabase() {
+        return db;
     }
 
 }
--- src/test/java/org/neo4j/tutorial/DoctorWhoUniverseTest.java
+++ src/test/java/org/neo4j/tutorial/DoctorWhoUniverseTest.java
@@ -1,11 +1,37 @@
 package org.neo4j.tutorial;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
 
 
 public class DoctorWhoUniverseTest {
+    
+    private GraphDatabaseService doctorWhoDatabase;
+    
+    @Before
+    public void startDatabase() {
+        doctorWhoDatabase = new DoctorWhoUniverse().getDoctorWhoDatabase();
+    }
+    
+    @After
+    public void stopDatabase() {
+        doctorWhoDatabase.shutdown();
+    }
+    
     @Test
-    public void seeIfTheJsonLoads() {
-        new DoctorWhoUniverse();
+    public void shouldHave11Doctors() {
+        int numberOfDoctors = 11;
+        int referenceNodeCount = 1;
+        assertEquals(numberOfDoctors + referenceNodeCount, DatabaseHelper.countNodes(doctorWhoDatabase.getAllNodes()));
+        
+    }
+    
+    @Test
+    public void shouldBeTenRegenerationRelationshipsBetweenTheElevenDoctors() {
+        
     }
 }
