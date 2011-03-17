218d14f654e8327ae7f4872968bc1174be083030
Jim Webber <jim@neotechnology.com>
2011-03-17 14:29:23 +0100

    Updated the sed script to use src/koan rather than src/koans after a previous refactoring

--- src/main/scripts/remove_snippets.sh
+++ src/main/scripts/remove_snippets.sh
@@ -2,4 +2,4 @@
 
 # Script to remove any code between //SNIPPET_START and //SNIPPET_END in Java unit test files
 
-find src/koans/java -name *.java -print | xargs sed -f src/main/scripts/remove_snippets.sed -i ""
+find src/koan/java -name *.java -print | xargs sed -f src/main/scripts/remove_snippets.sed -i ""
