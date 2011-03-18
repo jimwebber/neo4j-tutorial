e755e1de07d26ceb82788ee37769f19e388d5129
Jim Webber <jim@jimwebber.org>
2011-03-18 16:10:25 +0100

    Timelords are now represented as a species, the only one to come from Galifrey

--- src/test/java/org/neo4j/tutorial/DoctorWhoUniverseTest.java
+++ src/test/java/org/neo4j/tutorial/DoctorWhoUniverseTest.java
@@ -2,6 +2,7 @@ package org.neo4j.tutorial;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.neo4j.tutorial.DoctorWhoUniverse.PLAYED;
 
 import java.util.List;
 
@@ -9,15 +10,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.DynamicRelationshipType;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 
 public class DoctorWhoUniverseTest {
-
-    private static final DynamicRelationshipType REGNERATED_TO = DynamicRelationshipType.withName("REGENERATED_TO");
-    private static final DynamicRelationshipType PLAYED = DynamicRelationshipType.withName("PLAYED");
     
     private DoctorWhoUniverse doctorWhoUniverse = new DoctorWhoUniverse();
     private GraphDatabaseService doctorWhoDatabase;
@@ -58,7 +55,7 @@ public class DoctorWhoUniverseTest {
     private int countRelationships(Node timelord) {
         int regenerationCount = 0;
         while (true) {
-            List<Relationship> relationships = DatabaseHelper.toList(timelord.getRelationships(REGNERATED_TO,
+            List<Relationship> relationships = DatabaseHelper.toList(timelord.getRelationships(DoctorWhoUniverse.REGENERATED_TO,
                     Direction.OUTGOING));
 
             if (relationships.size() == 1) {
@@ -86,4 +83,25 @@ public class DoctorWhoUniverseTest {
         assertNotNull(doctorWhoUniverse.getIndex().getSingleNode("timelord-name", "Master"));
         assertNotNull(doctorWhoUniverse.getIndex().getSingleNode("timelord-name", "Doctor"));
     }
+    
+    @Test
+    public void doctorAndMasterAreTimelords() {
+        Node timelord = doctorWhoUniverse.getIndex().getSingleNode("species", "Timelord");
+        
+        Iterable<Relationship> relationships = timelord.getRelationships(DoctorWhoUniverse.IS_A, Direction.INCOMING);
+        
+        int numberOfTimelords = 2;
+        assertEquals(numberOfTimelords, DatabaseHelper.countRelationships(relationships));
+    }
+    
+    @Test
+    public void timelordsAreFromGalifrey() {
+        Node planet = doctorWhoUniverse.getIndex().getSingleNode("planet", "Galifrey");
+       
+        Iterable<Relationship> relationships = planet.getRelationships(DoctorWhoUniverse.FROM, Direction.INCOMING);
+        
+        int numberOfSpeciesFromGalifrey = 1;
+        assertEquals(numberOfSpeciesFromGalifrey, DatabaseHelper.countRelationships(relationships));
+    }
+    
 }
