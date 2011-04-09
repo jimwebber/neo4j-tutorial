8a049e98a6e605212dae27830c70bd15787cc5de
Jim Webber <jim@jimwebber.org>
2011-04-09 02:48:37 +0100

    Partially complete major refactoring of the way data is loaded into the graph. Some slides updated to reflect the new graph structure.

---
+++ src/main/java/org/neo4j/tutorial/Enemies.java
@@ -0,0 +1,51 @@
+package org.neo4j.tutorial;
+
+import java.io.File;
+import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.index.Index;
+
+public class Enemies extends Characters {
+
+    private Map<String, Map<String, String>> enemiesMap;
+    private final Node theDoctor;
+
+    @SuppressWarnings("unchecked")
+    public Enemies(Node theDoctor, File data) {
+        this.theDoctor = theDoctor;
+        ObjectMapper m = new ObjectMapper();
+        try {
+            enemiesMap = m.readValue(data, Map.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertAndIndex(GraphDatabaseService db, Index<Node> enemiesIndex, Index<Node> speciesIndex, Index<Node> planetIndex) {
+        Transaction tx = db.beginTx();
+        try {
+            for (String friendlyName : enemiesMap.keySet()) {
+                Node enemyNode = db.createNode();
+                enemyNode.setProperty("name", friendlyName);
+                enemiesIndex.add(enemyNode, "name", friendlyName);
+
+                connectToSpecies(enemyNode, enemiesMap.get(friendlyName), speciesIndex);
+                Species.connectToHomeworld(enemyNode, enemiesMap.get(friendlyName), planetIndex);
+                makeEnemyOfDoctor(enemyNode);
+            }
+
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+    }
+
+    private void makeEnemyOfDoctor(Node enemyNode) {
+        enemyNode.createRelationshipTo(theDoctor, DoctorWhoUniverse.ENEMY_OF);
+    }
+}
---
+++ src/main/java/org/neo4j/tutorial/Species.java
@@ -0,0 +1,60 @@
+package org.neo4j.tutorial;
+
+import java.io.File;
+import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
+
+public class Species {
+
+    private Map<String, Map<String, String>> speciesMap;
+
+    @SuppressWarnings("unchecked")
+    public Species(File data) {
+        ObjectMapper m = new ObjectMapper();
+        try {
+            speciesMap = m.readValue(data, Map.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertAndIndex(GraphDatabaseService db, Index<Node> speciesIndex, final Index<Node> planetIndex) {
+        Transaction tx = db.beginTx();
+        try {
+            for (String speciesName : speciesMap.keySet()) {
+                Node speciesNode = db.createNode();
+                speciesNode.setProperty("species", speciesName);
+                speciesIndex.add(speciesNode, "species", speciesName);
+
+                connectToHomeworld(speciesNode, speciesMap.get(speciesName), planetIndex);
+            }
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+    }
+    
+    public static void connectToHomeworld(Node speciesNode, Map<String, String> map, Index<Node> planetIndex) {
+        if(map == null || !map.containsKey("planet")) {
+            return; // no planet known for this species
+        }
+        
+        String planetName = map.get("planet");
+        IndexHits<Node> indexHits = planetIndex.get("planet", planetName);
+        if(indexHits != null) {
+            Node planet = indexHits.getSingle();
+            if(planet != null) {
+                speciesNode.createRelationshipTo(planet, DoctorWhoUniverse.FROM);
+            }
+        } else {
+            throw new RuntimeException(String.format("Planet [%s] is not known in the Doctor Who universe, unable to add FROM relationship for species [%s]", planetName, speciesNode.getProperty("species")));
+        }
+    }
+}
