77f420f9375d004ae3b57940cedc9b582a61ca6b
Jim Webber <jim@neotechnology.com>
2011-05-01 17:47:56 +0100

    Added the simplest Koan that uses the simple traversal framework - search for companions of the Doctor.

--- src/koan/java/org/neo4j/tutorial/Koan05.java
+++ src/koan/java/org/neo4j/tutorial/Koan05.java
@@ -1,10 +1,21 @@
 package org.neo4j.tutorial;
 
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
 import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.ReturnableEvaluator;
+import org.neo4j.graphdb.StopEvaluator;
+import org.neo4j.graphdb.Traverser;
+import org.neo4j.graphdb.Traverser.Order;
 
 /**
- * In this Koan we start using the simple traversal framework to find interesting information
- * from the graph.
+ * In this Koan we start using the simple traversal framework to find
+ * interesting information from the graph.
  */
 public class Koan05 {
 
@@ -15,9 +26,25 @@ public class Koan05 {
 
         universe = new DoctorWhoUniverse();
     }
-    
+
     @Test
     public void shouldFindAllCompanions() {
-        
+        Node theDoctor = universe.theDoctor();
+        Traverser t = null;
+
+        // SNIPPET_START
+
+        t = theDoctor.traverse(Order.DEPTH_FIRST,
+                StopEvaluator.DEPTH_ONE,
+                ReturnableEvaluator.ALL_BUT_START_NODE,
+                DoctorWhoUniverse.COMPANION_OF,
+                Direction.INCOMING);
+
+        // SNIPPET_END
+
+        Collection<Node> foundCompanions = t.getAllNodes();
+
+        int knownNumberOfCompanions = 42;
+        assertEquals(knownNumberOfCompanions, foundCompanions.size());
     }
 }
