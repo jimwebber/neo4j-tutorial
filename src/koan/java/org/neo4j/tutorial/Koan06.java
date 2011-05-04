efd61f5ff98140884a25137b9f0f31c7a1e7bd65
Jim Webber <jim@neotechnology.com>
2011-05-04 12:13:31 +0100

    Allowed characters to belong to more than one species, to account for the case of Miss Hartigan who was turned into a cyberman

--- src/koan/java/org/neo4j/tutorial/Koan06.java
+++ src/koan/java/org/neo4j/tutorial/Koan06.java
@@ -3,6 +3,14 @@ package org.neo4j.tutorial;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
+import org.neo4j.graphdb.traversal.Evaluation;
+import org.neo4j.graphdb.traversal.Evaluator;
+import org.neo4j.graphdb.traversal.Traverser;
+import org.neo4j.kernel.Traversal;
+import org.neo4j.kernel.Uniqueness;
 
 /**
  * In this Koan we start using the new traversal framework to find interesting
@@ -24,5 +32,15 @@ public class Koan06 {
 
     @Test
     public void shouldFindTheLoveRivalOfRiver() throws Exception {
+//        Node riverSong = universe.characterIndex.get("name", "River Song").getSingle();
+//        
+//        Traverser traverser = Traversal.description().expand(Traversal.expanderForTypes(DoctorWhoUniverse.LOVES, Direction.BOTH)).depthFirst()..evaluator(new Evaluator() {
+//            
+//            @Override
+//            public Evaluation evaluate(Path path) {
+//                // TODO Auto-generated method stub
+//                return null;
+//            }
+//        }).uniqueness(Uniqueness.NODE_GLOBAL).traverse(riverSong);
     }
 }
