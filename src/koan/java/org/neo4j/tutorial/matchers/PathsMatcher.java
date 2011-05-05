ad8ef000130680a2068f28f3b60e3b8894de988d
Jim Webber <jim@neotechnology.com>
2011-05-05 16:45:35 +0100

    Added graph-matching as an ivy dependency

---
+++ src/koan/java/org/neo4j/tutorial/matchers/PathsMatcher.java
@@ -0,0 +1,66 @@
+package org.neo4j.tutorial.matchers;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.junit.internal.matchers.TypeSafeMatcher;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
+
+public class PathsMatcher extends TypeSafeMatcher<Iterable<Path>> {
+    private final Set<Node> nodes;
+    private final Node start;
+    private final Node end;
+    
+
+    private PathsMatcher(Node start, Set<Node> nodes, Node end) {
+        this.start = start;
+        this.nodes = nodes;
+        this.end = end;
+        
+    }
+    
+    @Override
+    public void describeTo(Description description) {
+        description.appendText(String.format("BETTER DESCRIPTION NEEDED"));
+    }
+    
+    @Override
+    public boolean matchesSafely(Iterable<Path> paths) {
+        int numberOfPaths = 0;
+        for(Path p : paths) {
+            numberOfPaths ++;
+            boolean middleNodeFound = false;
+            for(Node middle : nodes) {
+                if(nodesAreInPath(p, start, middle, end)) {
+                    middleNodeFound = true;
+                    break;
+                }
+            }
+            
+            if(!middleNodeFound) {
+                return false;
+            }
+        }
+        
+        return numberOfPaths == nodes.size();
+    }
+    
+    private boolean nodesAreInPath(Path p, Node start, Node middle, Node end) {
+        for(Node n : p.nodes()) {
+            if(!(n.equals(start) || n.equals(end) || n.equals(middle))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    @Factory
+    public static <T> Matcher<Iterable<Path>> consistPreciselyOf(Node start, Set<Node> nodes, Node end) {
+      return new PathsMatcher(start, nodes, end);
+    }
+}
