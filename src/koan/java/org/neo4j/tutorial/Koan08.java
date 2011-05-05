be2ae8e97fcae2b0e3f7b7ede3137065e724f532
Jim Webber <jim@neotechnology.com>
2011-05-05 16:36:38 +0100

    Added the graph matching jar and matching koan. The jar is needed because neo4j/graph-matching is not available through the MVN repo.

---
+++ src/koan/java/org/neo4j/tutorial/Koan08.java
@@ -0,0 +1,44 @@
+package org.neo4j.tutorial;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.neo4j.tutorial.matchers.ContainsOnlySpecificNodes.containsOnly;
+import static org.neo4j.tutorial.matchers.PathsMatcher.consistPreciselyOf;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.neo4j.graphalgo.GraphAlgoFactory;
+import org.neo4j.graphalgo.PathFinder;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
+import org.neo4j.kernel.Traversal;
+
+/**
+ * In this Koan we use the graph-matching library to look for patterns in the
+ * Doctor's universe.
+ */
+public class Koan08 {
+
+    private static DoctorWhoUniverse universe;
+
+    @BeforeClass
+    public static void createDatabase() throws Exception {
+        universe = new DoctorWhoUniverse();
+    }
+
+    @AfterClass
+    public static void closeTheDatabase() {
+        universe.stop();
+    }
+
+    @Test
+    public void shouldFindDoctorsThatFoughtMoreThanOneEnemyConcurrently() {
+        
+    }
+}
