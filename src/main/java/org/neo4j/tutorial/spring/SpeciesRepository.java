089ee8561db66e6e64a61929d5574b64714e17f8
Ian Robinson <iansrobinson@gmail.com>
2011-05-08 20:55:45 +0200

    Deleted SpeciesRepository - we're not currently doing SDG, and this file is causing errors in IDE

--- src/main/java/org/neo4j/tutorial/spring/SpeciesRepository.java
+++
@@ -1,7 +0,0 @@
-package org.neo4j.tutorial.spring;
-
-import org.springframework.data.graph.neo4j.repository.GraphRepository;
-
-public interface SpeciesRepository extends GraphRepository<SpeciesEntity> {
-
-}
