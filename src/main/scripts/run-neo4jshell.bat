80aec46a2cc107afde6172d196129cbbc5af6a5a
Ian Robinson <iansrobinson@gmail.com>
2011-05-10 10:14:00 +0200

    Removed extensions from jars in neo shell scripts

--- src/main/scripts/run-neo4jshell.bat
+++ src/main/scripts/run-neo4jshell.bat
@@ -4,5 +4,5 @@ set PROJECT_DIR=Z:\MYDO~0F4\neo\development\neo4j-tutorial
 set LIB_DIR=%PROJECT_DIR%\lib
 
 
-java -cp %LIB_DIR%\neo4j-shell.jar;%LIB_DIR%\neo4j-kernel-1.3.jar;%LIB_DIR%\geronimo-jta_1.1_spec-1.1.1.jar org.neo4j.shell.StartClient -path %1
+java -cp %LIB_DIR%\neo4j-shell.jar;%LIB_DIR%\neo4j-kernel.jar;%LIB_DIR%\geronimo-jta_1.1_spec.jar org.neo4j.shell.StartClient -path %1
  
