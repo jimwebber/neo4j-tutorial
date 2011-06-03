954414955990fec57077fbf2f13cc4981a8475a9
Ian Robinson <iansrobinson@gmail.com>
2011-06-03 16:03:56 +0100

    First REST test - count the enemies of the Doctor

--- src/main/java/org/neo4j/tutorial/ServerDoctorWhoUniverse.java
+++ src/main/java/org/neo4j/tutorial/ServerDoctorWhoUniverse.java
@@ -1,31 +1,79 @@
 package org.neo4j.tutorial;
 
-import org.neo4j.graphdb.Node;
-import org.neo4j.server.NeoServer;
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+
+import org.neo4j.server.NeoServerWithEmbeddedWebServer;
+import org.neo4j.server.modules.DiscoveryModule;
+import org.neo4j.server.modules.ManagementApiModule;
+import org.neo4j.server.modules.RESTApiModule;
+import org.neo4j.server.modules.ThirdPartyJAXRSModule;
+import org.neo4j.server.modules.WebAdminModule;
+import org.neo4j.server.rest.domain.JsonHelper;
+import org.neo4j.server.rest.domain.JsonParseException;
 import org.neo4j.tutorial.server.ServerBuilder;
+import org.neo4j.tutorial.server.rest.FunctionalTestHelper;
 
-public class ServerDoctorWhoUniverse extends DoctorWhoUniverse {
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 
-	private final NeoServer server;
+public class ServerDoctorWhoUniverse extends DoctorWhoUniverse<Map<String,Object>> {
 
+	private final NeoServerWithEmbeddedWebServer server;
+
+	@SuppressWarnings("unchecked")
 	public ServerDoctorWhoUniverse() throws Exception {
 		super();
-		server = ServerBuilder.server()
+		server = ServerBuilder
+				.server()
 				.usingDatabaseDir(getDatabaseDirectory())
-				.withPassingStartupHealthcheck().withDefaultDatabaseTuning()
+				.withPassingStartupHealthcheck()
+				.withDefaultDatabaseTuning()
+				.withSpecificServerModules(DiscoveryModule.class,
+						RESTApiModule.class, ManagementApiModule.class,
+						ThirdPartyJAXRSModule.class, WebAdminModule.class)
 				.build();
 		server.start();
 	}
 
 	@Override
-	Node theDoctor() {
-		// TODO Auto-generated method stub
-		return null;
+	Map<String,Object> theDoctor() {
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource resource = client.resource(getDoctorUri());
+		String response = resource.accept(MediaType.APPLICATION_JSON).get(
+				String.class);
+		try {
+			return JsonHelper.jsonToMap(response);
+		} catch (JsonParseException e) {
+			throw new RuntimeException("Invalid response when looking up Doctor node");
+		}
+	}
+
+	private String getDoctorUri() {
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource resource = client.resource(new FunctionalTestHelper(server)
+				.indexNodeUri("characters", "name", "Doctor"));
+		String response = resource.accept(MediaType.APPLICATION_JSON).get(
+				String.class);
+		try {
+			return JsonHelper.jsonToList(response).get(0).get("self").toString();
+		} catch (JsonParseException e) {
+			throw new RuntimeException("Invalid response when looking up Doctor node");
+		}
 	}
 
 	@Override
 	void stop() {
 		server.stop();
+	}
+
+	public NeoServerWithEmbeddedWebServer getServer() {
+		return server;
 	}
 
 }
---
+++ src/main/java/org/neo4j/tutorial/server/rest/FunctionalTestHelper.java
@@ -0,0 +1,128 @@
+package org.neo4j.tutorial.server.rest;
+
+import org.neo4j.kernel.AbstractGraphDatabase;
+import org.neo4j.server.NeoServerWithEmbeddedWebServer;
+import org.neo4j.server.rest.domain.JsonHelper;
+import org.neo4j.server.rest.domain.JsonParseException;
+
+import java.io.IOException;
+
+public final class FunctionalTestHelper {
+    private final NeoServerWithEmbeddedWebServer server;
+
+    public FunctionalTestHelper(NeoServerWithEmbeddedWebServer server) {
+        if (server.getDatabase() == null) {
+            throw new RuntimeException("Server must be started before using " + getClass().getName());
+        }
+        this.server = server;
+    }
+
+    void assertLegalJson(String entity) throws IOException, JsonParseException {
+        JsonHelper.jsonToMap(entity);
+    }
+
+    public String dataUri() {
+        return server.baseUri().toString() + "db/data/";
+    }
+
+    public String nodeUri() {
+        return dataUri() + "node";
+    }
+
+    public String nodeUri(long id) {
+        return nodeUri() + "/" + id;
+    }
+
+    public String nodePropertiesUri(long id) {
+        return nodeUri(id) + "/properties";
+    }
+
+    public String nodePropertyUri(long id, String key) {
+        return nodePropertiesUri(id) + "/" + key;
+    }
+
+    public String relationshipUri() {
+        return dataUri() + "relationship";
+    }
+
+    public String relationshipUri(long id) {
+        return relationshipUri() + "/" + id;
+    }
+
+    public String relationshipPropertiesUri(long id) {
+        return relationshipUri(id) + "/properties";
+    }
+
+    public String relationshipPropertyUri(long id, String key) {
+        return relationshipPropertiesUri(id) + "/" + key;
+    }
+
+    public String relationshipsUri(long nodeId, String dir, String... types) {
+        StringBuilder typesString = new StringBuilder();
+        for (String type : types) {
+            typesString.append(typesString.length() > 0 ? "&" : "");
+            typesString.append(type);
+        }
+        return nodeUri(nodeId) + "/relationships/" + dir + "/" + typesString;
+    }
+
+    public String indexUri() {
+        return dataUri() + "index/";
+    }
+
+    public String nodeIndexUri() {
+        return indexUri() + "node/";
+    }
+
+    public String relationshipIndexUri() {
+        return indexUri() + "relationship/";
+    }
+
+    public String mangementUri() {
+        return server.baseUri().toString() + "db/manage";
+    }
+
+    public String indexNodeUri(String indexName) {
+        return nodeIndexUri() + indexName;
+    }
+
+    public String indexRelationshipUri(String indexName) {
+        return relationshipIndexUri() + indexName;
+    }
+
+    public String indexNodeUri(String indexName, String key, Object value) {
+        return indexNodeUri(indexName) + "/" + key + "/" + value;
+    }
+
+    public String indexRelationshipUri(String indexName, String key, Object value) {
+        return indexRelationshipUri(indexName) + "/" + key + "/" + value;
+    }
+
+    public String extensionUri() {
+        return dataUri() + "ext";
+    }
+
+    public String extensionUri(String name) {
+        return extensionUri() + "/" + name;
+    }
+
+    public String graphdbExtensionUri(String name, String method) {
+        return extensionUri(name) + "/graphdb/" + method;
+    }
+
+    public String nodeExtensionUri(String name, String method, long id) {
+        return extensionUri(name) + "/node/" + id + "/" + method;
+    }
+
+    public String relationshipExtensionUri(String name, String method, long id) {
+        return extensionUri(name) + "/relationship/" + id + "/" + method;
+    }
+
+    public AbstractGraphDatabase getDatabase() {
+        return server.getDatabase().graph;
+    }
+
+    public String getWebadminUri() {
+        return server.baseUri().toString() + "webadmin";
+    }
+}
