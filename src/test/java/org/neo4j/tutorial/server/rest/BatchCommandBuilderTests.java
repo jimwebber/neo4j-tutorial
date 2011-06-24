21e7b93616b8ebb8fd924c1f528e2dce12ab4b75
Jim Webber <jim@jimwebber.org>
2011-06-24 22:48:41 +0100

    Sorting out imports.

--- src/test/java/org/neo4j/tutorial/server/rest/BatchCommandBuilderTests.java
+++ src/test/java/org/neo4j/tutorial/server/rest/BatchCommandBuilderTests.java
@@ -1,6 +1,6 @@
 package org.neo4j.tutorial.server.rest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.neo4j.helpers.collection.MapUtil;
