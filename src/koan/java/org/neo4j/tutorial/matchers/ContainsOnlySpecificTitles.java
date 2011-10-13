26975ad30e92243e3671d7fa56d158360e19b2e5
Tobias Ivarsson <tobias@thobe.org>
2011-10-13 17:13:18 +0200

    Revert "Starting to upgrade the Koans to the most recent Neo4j version"
    Cypher contains a pretty nasty bug in that version.
    
    This reverts commit 23299a84baa0a6eb53934d6b45c3bb37d693c91d.

--- src/koan/java/org/neo4j/tutorial/matchers/ContainsOnlySpecificTitles.java
+++ src/koan/java/org/neo4j/tutorial/matchers/ContainsOnlySpecificTitles.java
@@ -22,30 +22,20 @@ public class ContainsOnlySpecificTitles extends TypeSafeMatcher<Iterable<Node>> 
     }
 
     @Override
-    public void describeTo( Description description )
-    {
-        if ( failedNode == null )
-        {
-            description.appendText( "missing titles: " + titles );
-        }
-        else
-        {
-            description.appendText( String.format(
-                    "Node [%d] (title=\"%s\") does not contain any of the specified titles", failedNode.getId(),
-                    failedNode.getProperty( "title", "<NONE>" ) ) );
-        }
+    public void describeTo(Description description) {
+        description.appendText(String.format("Node [%d] does not contain any of the specified titles", failedNode.getId()));
     }
 
     @Override
     public boolean matchesSafely(Iterable<Node> candidateNodes) {
-
+        
         for (Node n : candidateNodes) {
             String property = String.valueOf(n.getProperty("title"));
-
+            
             if (!titles.contains(property)) {
                 failedNode = n;
                 return false;
-            }
+            } 
             titles.remove(property);
         }
 
