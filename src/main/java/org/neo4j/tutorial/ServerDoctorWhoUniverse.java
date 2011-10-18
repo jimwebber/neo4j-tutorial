59d2ab93d38d3d449ae3d82154adb546e7057fe5
Peter Neubauer <peter@neubauer.se>
2011-10-18 23:03:13 +0200

    finally getting classifier to work

--- src/main/java/org/neo4j/tutorial/ServerDoctorWhoUniverse.java
+++ src/main/java/org/neo4j/tutorial/ServerDoctorWhoUniverse.java
@@ -15,70 +15,56 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
-public class ServerDoctorWhoUniverse
-{
+public class ServerDoctorWhoUniverse {
 
-    private final NeoServerWithEmbeddedWebServer server;
+	private final NeoServerWithEmbeddedWebServer server;
 
-    public ServerDoctorWhoUniverse( DoctorWhoUniverseGenerator universe ) throws Exception
-    {
-        super();
-        server = ServerBuilder.server()
-                .usingDatabaseDir( universe.getDatabaseDirectory() )
-                .build();
-        server.start();
-    }
+	public ServerDoctorWhoUniverse(DoctorWhoUniverseGenerator universe) throws Exception {
+		super();
+		server = ServerBuilder
+				.server()
+				.usingDatabaseDir(universe.getDatabaseDirectory())
+				.build();
+		server.start();
+	}
 
-    public Map<String, Object> theDoctor()
-    {
-        return getJsonFor( getUriFromIndex( "characters", "character", "Doctor" ) );
-    }
+	public Map<String,Object> theDoctor() {
+		return getJsonFor(getUriFromIndex("characters", "name", "Doctor"));
+	}
+	
+	public Map<String,Object> getJsonFor(String uri){
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource resource = client.resource(uri);
+		String response = resource.accept(MediaType.APPLICATION_JSON).get(
+				String.class);
+		try {
+			return JsonHelper.jsonToMap(response);
+		} catch (JsonParseException e) {
+			throw new RuntimeException("Invalid response when looking up Doctor node");
+		}
+	}
+	
+	public String getUriFromIndex(String indexName, String key, String value) {
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource resource = client.resource(new FunctionalTestHelper(server)
+				.indexNodeUri(indexName, key, value));
+		String response = resource.accept(MediaType.APPLICATION_JSON).get(
+				String.class);
+		try {
+			return JsonHelper.jsonToList(response).get(0).get("self").toString();
+		} catch (JsonParseException e) {
+			throw new RuntimeException("Invalid response when looking up node");
+		}
+	}
 
-    public Map<String, Object> getJsonFor( String uri )
-    {
-        ClientConfig config = new DefaultClientConfig();
-        Client client = Client.create( config );
-        WebResource resource = client.resource( uri );
-        String response = resource.accept( MediaType.APPLICATION_JSON )
-                .get( String.class );
-        try
-        {
-            return JsonHelper.jsonToMap( response );
-        }
-        catch ( JsonParseException e )
-        {
-            throw new RuntimeException( "Invalid response when looking up Doctor node" );
-        }
-    }
+	void stop() {
+		server.stop();
+	}
 
-    public String getUriFromIndex( String indexName, String key, String value )
-    {
-        ClientConfig config = new DefaultClientConfig();
-        Client client = Client.create( config );
-        WebResource resource = client.resource( new FunctionalTestHelper( server ).indexNodeUri( indexName, key, value ) );
-        String response = resource.accept( MediaType.APPLICATION_JSON )
-                .get( String.class );
-        try
-        {
-            return JsonHelper.jsonToList( response )
-                    .get( 0 )
-                    .get( "self" )
-                    .toString();
-        }
-        catch ( JsonParseException e )
-        {
-            throw new RuntimeException( "Invalid response when looking up node" );
-        }
-    }
-
-    void stop()
-    {
-        server.stop();
-    }
-
-    public NeoServerWithEmbeddedWebServer getServer()
-    {
-        return server;
-    }
+	public NeoServerWithEmbeddedWebServer getServer() {
+		return server;
+	}
 
 }
