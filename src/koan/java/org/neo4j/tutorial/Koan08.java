75d998bdf56a1c1e12f7729cea6525aa3f0e9f0d
jimwebber <jim@jimwebber.org>
2011-11-03 22:40:18 +0000

    tidied up the cypher koan

--- src/koan/java/org/neo4j/tutorial/Koan08.java
+++ src/koan/java/org/neo4j/tutorial/Koan08.java
@@ -48,7 +48,7 @@ public class Koan08
         // YOUR CODE GOES HERE
         // SNIPPET_START
 
-        cql = "START daleks = node:Species(species =\"Dalek\") MATCH (daleks)-[:APPEARED_IN]->(episode) RETURN episode";
+        cql = "start daleks = node:species(species ='Dalek') match (daleks)-[:APPEARED_IN]->(episode) RETURN episode";
 
         // SNIPPET_END
 
@@ -75,7 +75,7 @@ public class Koan08
         // YOUR CODE GOES HERE
         // SNIPPET_START
 
-        cql = "start daleks = node:Species( species = \"Dalek\"), rose = node:Characters( character= \"Rose Tyler\"), tennant = node:actors( actor = \"David Tennant\")";
+        cql = "start daleks = node:species( species = 'Dalek'), rose = node:characters( character= 'Rose Tyler'), tennant = node:actors( actor = 'David Tennant')";
         cql += "match (tennant)-[:APPEARED_IN]->(ep), (rose)-[:APPEARED_IN]->(ep), (daleks)-[:APPEARED_IN]->(ep)";
         cql += "return ep";
 
@@ -106,7 +106,7 @@ public class Koan08
         //So, we must use prop.prop? - this fills the prop.prop column with a <null>
         //value for prop parts with no identifiable prop
 
-        cql = "start dalek  = node:Species( species = 'Dalek') ";
+        cql = "start dalek  = node:species( species = 'Dalek') ";
         cql += "match (dalek)-[:APPEARED_IN]->(episode)<-[:USED_IN]-(props)<-[:MEMBER_OF]-(prop) ";
         cql += "return prop.prop?, episode.episode order by episode.episode desc skip 4 limit 1";
 
@@ -130,8 +130,8 @@ public class Koan08
         // YOUR CODE GOES HERE
         // SNIPPET_START
 
-        cql = "START daleks= node:Species(species = \"Dalek\") MATCH (daleks)-[:APPEARED_IN]->(episode)<-[:USED_IN]-(props)<-[:MEMBER_OF]-(prop)"
-                + "-[:COMPOSED_OF]->(part)-[:ORIGINAL_PROP]->(originalprop) RETURN originalprop.prop, part.type, COUNT(episode.title)"
+        cql = "start daleks= node:species(species = 'Dalek') match (daleks)-[:APPEARED_IN]->(episode)<-[:USED_IN]-(props)<-[:MEMBER_OF]-(prop)"
+                + "-[:COMPOSED_OF]->(part)-[:ORIGINAL_PROP]->(originalprop) return originalprop.prop, part.type, COUNT(episode.title)"
                 + " ORDER BY COUNT(episode.title) DESC LIMIT 1";
 
         // SNIPPET_END
