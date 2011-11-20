32bb7d59c96764e069825ebb1ac66eaccd6263ce
jimwebber <jim@jimwebber.org>
2011-11-20 14:15:13 +0000

    Breaking out the cypher Koans

--- src/koan/java/org/neo4j/tutorial/Koan08c.java
+++ src/koan/java/org/neo4j/tutorial/Koan08c.java
@@ -39,57 +39,6 @@ public class Koan08
     }
 
     @Test
-    public void shouldFindAllTheEpisodesInWhichTheDaleksAppeared() throws Exception
-    {
-        CypherParser parser = new CypherParser();
-        ExecutionEngine engine = new ExecutionEngine( universe.getDatabase() );
-        String cql = null;
-
-        // YOUR CODE GOES HERE
-        // SNIPPET_START
-
-        cql = "start daleks = node:species(species ='Dalek') match (daleks)-[:APPEARED_IN]->(episode) RETURN episode";
-
-        // SNIPPET_END
-
-        Query query = parser.parse( cql );
-        ExecutionResult result = engine.execute( query );
-        Iterator<Node> episodes = result.javaColumnAs( "episode" );
-
-        assertThat( asIterable( episodes ),
-                containsOnlyTitles( "The Pandorica Opens", "Victory of the Daleks", "Journey's End", "The Stolen Earth", "Evolution of the Daleks",
-                        "Daleks in Manhattan", "Doomsday", "Army of Ghosts", "The Parting of the Ways", "Bad Wolf", "Dalek", "Remembrance of the Daleks",
-                        "Revelation of the Daleks", "Resurrection of the Daleks", "The Five Doctors", "Destiny of the Daleks", "Genesis of the Daleks", "Death to the Daleks",
-                        "Planet of the Daleks", "Frontier in Space", "Day of the Daleks", "The War Games", "The Evil of the Daleks", "The Power of the Daleks", "The Daleks' Master Plan", "The Chase",
-                        "The Space Museum", "The Dalek Invasion of Earth", "The Daleks" ) );
-
-    }
-
-    @Test
-    public void shouldFindEpisodesWhereTennantAndRoseBattleTheDaleks() throws Exception
-    {
-        CypherParser parser = new CypherParser();
-        ExecutionEngine engine = new ExecutionEngine( universe.getDatabase() );
-        String cql = null;
-
-        // YOUR CODE GOES HERE
-        // SNIPPET_START
-
-        cql = "start daleks = node:species( species = 'Dalek'), rose = node:characters( character= 'Rose Tyler'), tennant = node:actors( actor = 'David Tennant')";
-        cql += "match (tennant)-[:APPEARED_IN]->(ep), (rose)-[:APPEARED_IN]->(ep), (daleks)-[:APPEARED_IN]->(ep)";
-        cql += "return ep";
-
-        // SNIPPET_END
-
-        Query query = parser.parse( cql );
-        ExecutionResult result = engine.execute( query );
-        Iterator<Node> episodes = result.javaColumnAs( "ep" );
-
-        assertThat( asIterable( episodes ),
-                containsOnlyTitles( "Journey's End", "The Stolen Earth", "Doomsday", "Army of Ghosts", "The Parting of the Ways" ) );
-    }
-
-    @Test
     public void shouldFindTheFifthMostRecentPropToAppear() throws Exception
     {
         CypherParser parser = new CypherParser();
