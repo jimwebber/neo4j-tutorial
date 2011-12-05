85a96b73c54717dbcfce6a09f2bf9da18d16e7b2
jimwebber <jim@jimwebber.org>
2011-12-05 18:29:21 +1000

    Updating the readme after feedback from @ThornyDev

--- src/main/scripts/remove_snippets.sh
+++ src/main/scripts/remove_snippets.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # Script to remove any code between //SNIPPET_START and //SNIPPET_END in Java unit test files
+# Must be run from the top level Koan directory
 
 find src/koan/java -name *.java -print | xargs sed -f src/main/scripts/remove_snippets.sed -i ""
