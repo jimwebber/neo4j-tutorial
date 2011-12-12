10dd9924c89d2eee65f8e2062fd4afafb2bc8b4b
Michael <mpeterson2@gmail.com>
2011-12-11 19:03:53 -0500

    build.xml has specific koan targets; remove_snippets no err msg on Linux
    Made remove_snippets.sh compatible with sed options on Linux/cygwin

--- src/main/scripts/remove_snippets.sh
+++ src/main/scripts/remove_snippets.sh
@@ -3,4 +3,18 @@
 # Script to remove any code between //SNIPPET_START and //SNIPPET_END in Java unit test files
 # Must be run from the top level Koan directory
 
-find src/koan/java -name *.java -print | xargs sed -f src/main/scripts/remove_snippets.sed -i ""
+dashi='-i ""'  # default is version of sed that works on Mac OS X
+
+# sed on Linux/Cygwin is different and wants no space between -i and the arg
+# or can leave the arg off entirely if empty
+os=`uname`
+os_lwr=$(echo "$os" | sed 's/^\(\w\{5\}\).*/\1/' | tr '[A-Z]' '[a-z]')
+
+case "$os_lwr" in
+    "linux" )  dashi='-i' ;;
+    "cygwi" )  dashi='-i' ;;
+    "mingw" )  dashi='-i' ;;
+esac
+
+find src/koan/java -name *.java -print | xargs sed -f src/main/scripts/remove_snippets.sed $dashi
+
