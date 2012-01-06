989942d1d398e1fd36d297342856defc2ded013c
Jim Webber <jim@jimwebber.org>
2012-01-06 18:40:13 +0000

    Added awesomeness generator and tests as a basis for creating a managed extension koan

--- src/koan/java/org/neo4j/tutorial/koan14/UserNameAndPasswordForSalariesSecurityRule.java
+++ src/koan/java/org/neo4j/tutorial/koan14/UserNameAndPasswordForSalariesSecurityRule.java
@@ -1,5 +1,7 @@
 package org.neo4j.tutorial.koan14;
 
+import org.neo4j.cypher.ExecutionEngine;
+import org.neo4j.cypher.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.server.rest.security.SecurityRule;
 
@@ -19,6 +21,18 @@ public class UserNameAndPasswordForSalariesSecurityRule implements SecurityRule
                 httpServletRequest.getHeader(
                         "X-Password"));
         // SNIPPET_END
+
+        ExecutionEngine engine = new ExecutionEngine(graphDatabaseService);
+        String cql = null;
+
+        cql = "start char = node:characters(character = 'Rose Tyler')"
+                + "match (char)-[:COMES_FROM]->(planet)"
+                + "return planet.planet";
+
+        ExecutionResult result = engine.execute(cql);
+        String planet = (String) result.javaColumnAs("planet.planet").next();     
+        
+        System.out.println(planet);
 
         return loggedIn;
     }
