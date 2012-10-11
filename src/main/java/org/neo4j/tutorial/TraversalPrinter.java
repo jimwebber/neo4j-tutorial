42594c2fb219c7b110107b109a08d409c28192ce
iansrobinson <iansrobinson@gmail.com>
2012-10-11 08:24:38 +0200

    Updated traversal printer to give more hierarchical view

--- src/main/java/org/neo4j/tutorial/TraversalPrinter.java
+++ src/main/java/org/neo4j/tutorial/TraversalPrinter.java
@@ -1,5 +1,6 @@
 package org.neo4j.tutorial;
 
+import static java.lang.Math.min;
 import static java.util.Arrays.asList;
 
 import java.util.Iterator;
@@ -15,6 +16,8 @@ public class TraversalPrinter implements Evaluator
 {
     private final Iterable<String> propertyNames;
     private final Evaluator innerEvaluator;
+    private String lastPath = "";
+
 
     public TraversalPrinter( Evaluator innerEvaluator, String... propertyNames )
     {
@@ -24,18 +27,64 @@ public class TraversalPrinter implements Evaluator
 
     private void printPath( Path path )
     {
+        StringBuilder builder = new StringBuilder();
+        Node previousNode = null;
         for ( PropertyContainer propertyContainer : path )
         {
             if ( Node.class.isAssignableFrom( propertyContainer.getClass() ) )
             {
-                printNode( (Node) propertyContainer );
+                previousNode = (Node) propertyContainer;
+                builder.append( formatNode( previousNode ) );
             }
             else
             {
-                printRelationship( (Relationship) propertyContainer, path.endNode() );
+                builder.append( formatRelationship( (Relationship) propertyContainer, previousNode ) );
             }
         }
+        String currentPath = builder.toString();
+
+
+        String overlap = overlappingPath( currentPath, lastPath );
+        if ( !overlap.isEmpty() )
+        {
+            String formattedCurrentPath = repeat( " ", overlap.length() ).concat( currentPath.substring( overlap
+                    .length() ) );
+            System.out.print( formattedCurrentPath );
+        }
+        else
+        {
+            System.out.print( currentPath );
+        }
+
+
+        lastPath = currentPath;
+
     }
+
+    private String repeat( String s, int i )
+    {
+        return String.format( String.format( "%%0%dd", i ), 0 ).replace( "0", s );
+    }
+
+    private String overlappingPath( String s1, String s2 )
+    {
+        StringBuilder builder = new StringBuilder();
+        int maxLength = min( s1.length(), s2.length() );
+        for ( int i = 0; i < maxLength; i++ )
+        {
+            if ( s1.charAt( i ) == s2.charAt( i ) )
+            {
+                builder.append( s1.charAt( i ) );
+            }
+            else
+            {
+                break;
+            }
+        }
+        String overlap = builder.toString();
+        return overlap.substring( 0, overlap.lastIndexOf( ")" ) + 1 );
+    }
+
 
     private Evaluation printResult( Evaluation evaluation )
     {
@@ -44,43 +93,54 @@ public class TraversalPrinter implements Evaluator
     }
 
 
-    private void printRelationship( Relationship relationship, Node currentNode )
+    private String formatRelationship( Relationship relationship, Node previousNode )
     {
         String prefix = "";
         String suffix = "";
 
-        if ( relationship.getStartNode().equals( currentNode ) )
-        {
-            prefix = "<";
-        }
-        else
+        if ( relationship.getStartNode().equals( previousNode ) )
         {
             suffix = ">";
         }
+        else
+        {
+            prefix = "<";
+        }
 
-        System.out.print( String.format( "%s-[:%s]-%s", prefix, relationship.getType().name(), suffix ) );
+        return String.format( "%s-[:%s]-%s", prefix, relationship.getType().name(), suffix );
     }
 
-    private void printNode( Node node )
+    private String formatNode( Node node )
     {
-        System.out.print( "(" );
+        StringBuilder builder = new StringBuilder();
+        builder.append( "(" );
         boolean propertyFound = false;
-        Iterator<String> iterator = propertyNames.iterator();
-        while ( iterator.hasNext() && !propertyFound )
+        Iterator<String> propertyNameIterator = propertyNames.iterator();
+        while ( propertyNameIterator.hasNext() && !propertyFound )
         {
-            String propertyName = iterator.next();
+            String propertyName = propertyNameIterator.next();
             if ( node.hasProperty( propertyName ) )
             {
-                System.out.print( String.format( "%s:%s", propertyName, node.getProperty( propertyName ) ) );
+                builder.append( String.format( "%s:%s", propertyName, node.getProperty( propertyName ) ) );
                 propertyFound = true;
             }
         }
         if ( !propertyFound )
         {
-            System.out.print( String.format( "id:%s", node.getId() ) );
+            Iterator<String> propertyKeyIterator = node.getPropertyKeys().iterator();
+            if ( propertyKeyIterator.hasNext() )
+            {
+                String propertyName = propertyKeyIterator.next();
+                builder.append( String.format( "%s:%s", propertyName, node.getProperty( propertyName ) ) );
+            }
+            else
+            {
+                builder.append( String.format( "id:%s", node.getId() ) );
+            }
         }
 
-        System.out.print( ")" );
+        builder.append( ")" );
+        return builder.toString();
     }
 
     public Evaluation evaluate( Path path )
