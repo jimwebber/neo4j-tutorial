686889ded3dfa2fc30c67cf87fbb91022dd88f44
Jim Webber <jim@jimwebber.org>
2012-10-24 17:01:05 +0100

    Strengthening the make USB stick script a but

--- src/main/scripts/make-usb-sticks.sh
+++ src/main/scripts/make-usb-sticks.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-if [ -z $1 ] || [ -z $2 ] || [ -z $3 ];
+if [ `uname` != "Darwin" ];
+	  then echo "This sorry excuse for a script can only be run on Mac OSX, sorry.";
+	  exit 1;
+fi
+
+if [ -z $1 ] || [ -z $2 ]	;
 	then echo "Usage: make-usb-sticks <volume> <mount point > <tutorial source>";
-	echo "E.g. make-usb-sticks /dev/disk1 /Volumes/NEO4J ~/neo4j-tutorial";
+	echo "E.g. make-usb-sticks /dev/disk1 ~/neo4j-tutorial";
 	exit 1;
 fi
 
-diskutil unmount $1
-#diskutil eraseDisk "MS-DOS FAT32" NEO4J disk1
 diskutil eraseDisk "MS-DOS FAT32" NEO4J $1
-#cp -r ~/Desktop/neo4j-tutorial/ /Volumes/NEO4J/
-cp -r $3 $2
-diskutil unmountDisk $2
+echo diskutil mountDisk $1
+diskutil mountDisk $1
+
+cp -r $2 /Volumes/NEO4J
+
+diskutil unmountDisk /Volumes/NEO4J
 
 
