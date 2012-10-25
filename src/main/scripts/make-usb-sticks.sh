738aac5b52ff53d24428992e90c60d00bd9f523f
Jim Webber <jim@jimwebber.org>
2012-10-25 11:31:44 +0100

    More tweaks on the USB stick script

--- src/main/scripts/make-usb-sticks.sh
+++ src/main/scripts/make-usb-sticks.sh
@@ -11,12 +11,10 @@ if [ -z $1 ] || [ -z $2 ]	;
 	exit 1;
 fi
 
-#diskutil eraseVolume fat32 NEO4J disk3s1
-diskutil eraseVolume fat32 NEO4J $1
+diskutil eraseVolume fat32 NEO4J $1s1
+diskutil mountDisk $1
 
-#diskutil info disk3s1 | grep "Mount Point" | awk -F: {'print $2'}
-cp -r $2 `diskutil info $1 | grep "Mount Point" | awk -F: {'print $2'}`
+cp -r $2 "`diskutil info $1 | grep "Mount Point" | awk -F: {'print $2'}`"
 
 diskutil unmountDisk $1
-
 
