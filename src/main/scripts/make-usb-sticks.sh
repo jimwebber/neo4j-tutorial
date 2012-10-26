73c1626aecf7a019d70ce7b777079b0963c456ba
Jim Webber <jim@jimwebber.org>
2012-10-26 11:58:37 +0100

    Switch unmount for eject, adding some helpful progress output

--- src/main/scripts/make-usb-sticks.sh
+++ src/main/scripts/make-usb-sticks.sh
@@ -16,6 +16,8 @@ fi
 
 diskutil eraseDisk fat32 NEO4J MBR $1
 
+echo Disk $DISK formatted, copying content from $SOURCE
+
 if [ $? -ne 0 ];
 	then echo "Something went wrong creating or mounting the fat32 USB stick";
 	exit 1;
@@ -25,7 +27,7 @@ fi
 MOUNT_POINT=`diskutil info ${DISK}s1 | grep "Mount Point" | awk -F: {'print $2'} | sed 's/^ *//g'`
 cp -r $SOURCE "${MOUNT_POINT}"
 
-diskutil unmountDisk $1
+diskutil eject $1
 
 echo "Finished, please physically remove stick"
 echo $'\cg'
