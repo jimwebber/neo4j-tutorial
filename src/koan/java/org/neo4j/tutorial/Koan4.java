364f2ca68f0108d4f13d1f7172bc006c873ae26d
Jim Webber <jim@jimwebber.org>
2014-03-25 13:34:29 +0000

    Reordered koans to introduce match before properties and merge

--- src/koan/java/org/neo4j/tutorial/Koan4.java
+++ src/koan/java/org/neo4j/tutorial/Koan4.java
@@ -3,29 +3,36 @@ package org.neo4j.tutorial;
 import org.junit.Test;
 
 import org.neo4j.cypher.ExecutionEngine;
+import org.neo4j.cypher.ExecutionResult;
 import org.neo4j.kernel.impl.util.StringLogger;
 
+import static org.junit.Assert.assertEquals;
+
 /**
- * In this Koan we learn how to merge new nodes and relationships into an existing graph using
- * using the Cypher language.
+ * In this Koan we learn how to create, update, and erase properties and labels in Cypher.
  */
 public class Koan4
 {
-    // TODO: write an exercise
     @Test
-    public void shouldMERGE_ALL_THE_THINGS()
+    public void shouldCreateASingleProperty()
     {
         ExecutionEngine engine = new ExecutionEngine( DatabaseHelper.createDatabase(), StringLogger.DEV_NULL );
+
+        engine.execute( "CREATE ({actor: 'David Tennant'})" );
+
         String cql = null;
 
         // YOUR CODE GOES HERE
         // SNIPPET_START
 
-
+        cql = "CREATE n";
 
         // SNIPPET_END
 
         engine.execute( cql );
 
+        final ExecutionResult executionResult = engine.execute( "START n=node(*) return n" );
+
+        assertEquals( 1, executionResult.size() );
     }
 }
